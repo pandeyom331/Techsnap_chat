06165e18210b1bd8e17a7e0b4a595b34ec4a6498
Om pandey <54907469+pandeyom331@users.noreply.github.com>
2021-09-13 22:47:35 +0530

    Added Chat part

---
+++ TechSnap Chat/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import Login from "./Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {useStateValue} from "./StateProvider"
+
+function App() {
+
+  const [{user}] = useStateValue();
+
+  return (
+    // BEM naming convention
+    <div className="App">
+      {!user ? (<Login />) : 
+        (
+        <div div className="app__body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
