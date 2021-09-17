95aba2ce874b1b8bef1bf0823c36794bbfdf2efd
Om pandey <54907469+pandeyom331@users.noreply.github.com>
2021-09-17 13:23:16 +0530

    updating app.js for chat app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
 import SignUp from './components/SignUp';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { Route, Switch} from 'react-router-dom';
 import LoginIn from './components/Loginin';
 import GroupSection from './components/Groups/GroupSection';
+import ChatMain from './components/Chat/App'
+import { HashRouter as Router } from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>
          <Route path="/signup" component={SignUp} exact />
          <Route path="/" component={LoginIn} exact />
          <Route path="/welcome" component={GroupSection} exact />
+         <Route path="/chat" component={ChatMain} exact />
         </Switch>
       </Router>
     </div>
