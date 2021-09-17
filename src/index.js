cfbb6600c1fb03130b8ebdc6056f4a518a711919
Om pandey <54907469+pandeyom331@users.noreply.github.com>
2021-09-17 13:22:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import reducer, { initialState } from "./components/Chat/reducer";
+import { StateProvider } from "./components/Chat/StateProvider";
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
